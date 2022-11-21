9e3a4accfbb9941dc09c539b810bcab986eacaa7
Ruixue Luo <luorx1202@outlook.com>
2022-11-20 23:37:38 -0800

    Log in finished

--- bookingSystem.py
+++ bookingSystem.py
@@ -3,38 +3,51 @@ import sqlite3
 conn = sqlite3.connect('cinema.sqlite')
 cur = conn.cursor()
 
-cur.execute('SELECT * FROM User')
-print()
+isLoggedIn = False
 
 #Welcome
 print("\n********************WELCOME TO LALALAND MOVIE BOOKING TICKET SYSTEM***********************")
 #Sign up / in
 print("\n Please enter your email address and password; if you don't have an account with us, we will automatically create one for you ^_^")
 
-emailInput = input("\nPlease enter your email address:")
-print(emailInput)
-print(type(emailInput))
-password = input("\nPlease enter your password:")
-# check for email addr in User
-#cur.execute('''SELECT * FROM User WHERE email=(?);''',(emailInput,))
-cur.execute('SELECT id FROM User WHERE email = ? ', (emailInput,))
+while (isLoggedIn == False):
 
-if(len(list(cur)) == 0): 
-    print("you are right")
+    emailInput = input("\nPlease enter your email address: ")
 
-print(cur)
-user_password = cur.fetchone()
-print(user_password)
+    cur.execute('SELECT * FROM User WHERE email = ? ', (emailInput, ))
+    userFound = cur.fetchone()
 
+    #If there's no such user, we just create a new one
+    if (userFound == None): 
+        password = input("\nPlease set up your password: ")
+        cur.execute('INSERT INTO User (email, password) values (?, ?)', (emailInput, password))
+        isLoggedIn = True
+        print("\n------------A new account has been created!------------")
+    #If the user exists
+    else:
+        password = input("\nPlease enter your password:")
+        passwordFound = userFound[2]
+        #If the password is correct
+        if (password == passwordFound):
+            isLoggedIn = True
+            print("\n-----------Logged in successfully!-----------")
+        #Otherwise
+        else:
+            print("Wrong password! Please try again!")
 
-# pas = input("\nENTER YOUR PASSWORD:-")
-# otp = int(input("\nENTER A OTP CODE ON YOUR EMAIL AND PHONE NO:-"))
+#Buy tickets
+    #Return all genres
+    #Only 3 genres in our case
+        #Return all movies
+            #Return all available slots (isFull = 0)
+                #Return all seats available
+                #User select 
+                    #If the entered seat is not available, print something (this could be omitted if we assume every input is valid)
+                    #Otherwise, insert a new ticket and print detailed information
+                        #If SELECT count (*) from Ticket WHERE slot_id = XXXX is 9, 
+                        #UPDATE Slot SET isFull = 1 WHERE id = XXXXX
 
-# print("\n-------LOGIN SUCCESSFUL-------")
-
-
-
-# print("\n-------YOUR accountOUNT IS CREATED SUCCESSFULLY-------")
+#View tickets bought
 
 
 
