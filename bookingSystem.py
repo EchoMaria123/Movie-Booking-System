89afa18d076dc002308613e3bc16d9d7a6f82b4a
Mengya Zeng <echomaria98123@gmail.com>
2022-11-21 12:14:56 -0800

    select genre movie slot finished

--- bookingSystem.py
+++ bookingSystem.py
@@ -36,16 +36,60 @@ while (isLoggedIn == False):
             print("Wrong password! Please try again!")
 
 #Buy tickets
-    #Return all genres
-    #Only 3 genres in our case
-        #Return all movies
-            #Return all available slots (isFull = 0)
-                #Return all seats available
-                #User select 
-                    #If the entered seat is not available, print something (this could be omitted if we assume every input is valid)
-                    #Otherwise, insert a new ticket and print detailed information
-                        #If SELECT count (*) from Ticket WHERE slot_id = XXXX is 9, 
-                        #UPDATE Slot SET isFull = 1 WHERE id = XXXXX
+print("\n-----------Now time to book tickets!-----------")
+    
+genres = ['Romance', 'Comedy', 'Horror']
+#Return all genres
+print("\n-----------Please choose a preferred Genre from below:-----------")
+for genre in genres:
+    print("\n ", genre)
+while True:
+    genre_chosen = input("\n Please enter your chosen Genre: ")
+    if genre_chosen in genres:
+        break
+    else:
+        print("\n Wrong genre, please try again")
+
+#Return all movies
+movie_dic = {'Romance':['Me Before You', 'Titanic', 'About Time'], 'Comedy':['The Dictator', 'The Gold Rush', 'Four Weddings and a Funeral'], 'Horror':['Silent Hill', 'Saw', 'The Shining']}
+movies = movie_dic.get(genre_chosen)
+print("\n-----------Please choose a preferred Movie from below:-----------")
+for movie in movies:
+    print('\n', movie)
+while True:
+    movie_chosen = input("\n Please enter your chosen Movie: ")
+    if movie_chosen in movies:
+        break
+    else:
+        print("\n Wrong movie, please try again")
+
+#Return all available slots (isFull = 0)
+
+#get movie id
+cur.execute('SELECT id FROM Movie WHERE name = ? ', (movie_chosen, ))
+movie_id = cur.fetchone()[0]
+#get available slots
+cur.execute('SELECT * FROM Slot WHERE movie_id = ? AND isFull = 0', (movie_id, ))
+slots_avail = cur.fetchall()
+times = []
+print("\n-----------Please choose a preferred time slot from below:-----------")
+for slot_avail in slots_avail:
+    times.append(slot_avail[2])
+    print('\n', slot_avail[2])
+while True:
+    time_chosen = input("\n Please enter your chosen time slot: ")
+    if time_chosen in times:
+        break
+    else:
+        print("\n Wrong time slot, please try again")
+
+
+#Return all seats available
+#User select 
+    #If the entered seat is not available, print something (this could be omitted if we assume every input is valid)
+    #Otherwise, insert a new ticket and print detailed information
+        #If SELECT count (*) from Ticket WHERE slot_id = XXXX is 9, 
+        #UPDATE Slot SET isFull = 1 WHERE id = XXXXX
 
 #View tickets bought
 
